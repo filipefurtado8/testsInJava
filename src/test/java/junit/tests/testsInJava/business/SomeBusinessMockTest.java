3a479042cdfe144e2c9ff2f5e458997909e8dbe5
Filipe Furtado <filipe.furtado@mindswap.academy>
2023-01-07 15:21:22 +0000

    feat: add argumentCaptor to the tests

--- src/test/java/junit/tests/testsInJava/business/SomeBusinessMockTest.java
+++ src/test/java/junit/tests/testsInJava/business/SomeBusinessMockTest.java
@@ -8,6 +8,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -49,6 +52,7 @@ public class SomeBusinessMockTest {
 
         //Then
         assertEquals(expectedResult, actualResult);
+        verify(dataMock, times(1)).retrieveAllData();
     }
 
     @Test
@@ -61,20 +65,49 @@ public class SomeBusinessMockTest {
 
         //Then
         assertEquals(actualResult, expectedResult);
+        verify(dataMock, times(1)).retrieveAllData();
     }
 
     @Test
-    public void addValueToDatabase() {
+    public void addOneValueToDatabase() {
         //SUT
         Integer[] values = new Integer[]{1, 2, 3};
         dataMock.addValuesToDatabase(values);
 
         //Verification
         ArgumentCaptor<Integer[]> captor = ArgumentCaptor.forClass(Integer[].class);
-        verify(dataMock, times(1)).addValuesToDatabase(captor.capture());
+
+        //the capture method is used to save the value that you want to further evaluate with assertEquals
+        //in this case it saves the values variable(line 69) information
+        //since captor.capture() represents the values, the verify method also checks for how many times the addValuesToDatabase is used
+        verify(dataMock).addValuesToDatabase(captor.capture());
+
 
         //Here I'm verifying if the values inserted in the method are the same in the ArgumentCaptor
+        //the system.out.println demonstrates that
+        System.out.println(Arrays.toString(captor.getValue())); //captor.getValue -> [1,2,3]
         assertEquals(values, captor.getValue());
+
+    }
+
+    @Test
+    public void addMultipleValuesToDatabase() {
+        //SUT
+        Integer[] values = new Integer[]{1, 2, 3};
+        Integer[] values1 = new Integer[]{4, 5, 6};
+        dataMock.addValuesToDatabase(values);
+        dataMock.addValuesToDatabase(values1);
+
+        //Verification
+        ArgumentCaptor<Integer[]> captor = ArgumentCaptor.forClass(Integer[].class);
+
+        verify(dataMock, times(2)).addValuesToDatabase(captor.capture());
+
+        //this list is created to grab and simulate the values used in line 96 and 97
+        List<Integer[]> allValues = captor.getAllValues();
+
+        assertEquals(values, allValues.get(0));
+        assertEquals(values1, allValues.get(1));
 
     }
 
