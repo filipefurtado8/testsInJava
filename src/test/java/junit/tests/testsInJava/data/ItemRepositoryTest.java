7cd4cdec4931de6e4c695b067768240a39400089
Filipe Furtado <filipe.furtado@mindswap.academy>
2023-02-13 18:29:06 +0000

    feat: add data layer unit test

---
+++ src/test/java/junit/tests/testsInJava/data/ItemRepositoryTest.java
@@ -0,0 +1,26 @@
+package junit.tests.testsInJava.data;
+
+import junit.tests.testsInJava.model.Item;
+import junit.tests.testsInJava.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class ItemRepositoryTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void testFindAll() {
+        List<Item> items = itemRepository.findAll();
+        assertEquals(3, items.size());
+
+    }
+    
+}
